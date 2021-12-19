e32d3c0e637ebd51d5ba9a79eddf019c5f0b23fd
Brenda <careeropp101@gmail.com>
2021-12-19 09:38:00 +0300

    blog9

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
-    content = HTMLField()
+    content = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
