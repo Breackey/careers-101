304e727ab2b241311f53ca744f8dce82fac8527f
Brenda <careeropp101@gmail.com>
2021-12-19 09:50:41 +0300

    blog13

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'content')
