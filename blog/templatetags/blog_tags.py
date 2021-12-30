b148c320c1e67f64ec92c2ca625d749537e33ec4
Brenda <careeropp101@gmail.com>
2021-12-30 19:33:09 +0300

    added recent posts blog tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -10,16 +10,10 @@ register = template.Library()
 def markdown_format(text):
     return mark_safe(markdown.markdown(text))
 
-@register.simple_tag
-def total_posts():
-    return Post.published.count()
-
-   
-""" 
 @register.inclusion_tag('blog/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts': latest_posts} """
+    return {'latest_posts': latest_posts} 
 
 @register.simple_tag
 def show_latest_posts(count=5):
