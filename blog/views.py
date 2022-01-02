d8acc31bf14e424babb8fbdd8f63ec5e9a48e7fa
Brenda <careeropp101@gmail.com>
2022-01-02 19:08:35 +0300

    more blog changes

--- blog/views.py
+++ blog/views.py
@@ -49,7 +49,7 @@ def post_list(request, tag_slug=None):
     #context = {'page': page,'posts': posts,'tag': tag,'product_list' : productlist , 'category_list' : categorylist , 'count':Category.objects.count()}   
     
     return render(request,
-                  'blog/latest_posts.html',
+                  'blog/blog.html',
                   context)
    
     
