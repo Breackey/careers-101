b7f2c604068d66db8477258bc3e829a5a8b6f87c
Brenda <careeropp101@gmail.com>
2022-01-21 10:02:47 +0300

    added blog category

--- blog/migrations/pg_trgm.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ('blog', "0003_alter_comment_active"),
-    ]
-    operations = [
-        migrations.RunSQL('CREATE EXTENSION IF NOT EXISTS pg_trgm'),
-    ]
