f10a51244b1ad55d775d62773625954ea4d13e33
Brenda <careeropp101@gmail.com>
2022-01-28 06:47:09 +0300

    changed the saticfiles_dir

--- careers101/settings.py
+++ careers101/settings.py
@@ -182,13 +182,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, "media" )
 
 
-PROJECT_ROOT   =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/' 
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 
