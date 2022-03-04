d7e43ee111673844406f41c06483cdc48a8c0549
Brenda <careeropp101@gmail.com>
2022-03-04 14:27:02 +0300

    commented out dev db

--- careers101/settings.py
+++ careers101/settings.py
@@ -113,16 +113,16 @@ WSGI_APPLICATION = 'careers101.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
+""" if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     } 
-else:
+else: """
 
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'careers101db', 
